7c61fba2b6b201e10d5be73c7126e8108197be0c
Sara Martin <miss.iserah@gmail.com>
2020-05-19 22:42:41 -0500

    finished SerializeField vs public

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -24,6 +24,12 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
@@ -37,6 +43,11 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -46,5 +57,7 @@ public class Rocket : MonoBehaviour
         {
             transform.Rotate(-Vector3.forward);
         }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
 }
