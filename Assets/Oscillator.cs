ac1d68db8676582faa11ca362d8298e76153ee43
Sara Martin <miss.iserah@gmail.com>
2020-05-20 23:47:12 -0500

    finished mathf.sin() for movement cycles

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f,10f,10f);
+    [SerializeField] float period = 2f;
 
     //todo remove from inspector
     [Range(0,1)]
@@ -24,6 +25,17 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // set movement factor
+
+        float cycles = Time.time / period;  // grows continually from 0
+
+        const float tau = Mathf.PI * 2;  // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        print(rawSinWave);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+
+
         Vector3 offset = movementFactor * movementVector;
         transform.position = startingPos + offset;
     }
